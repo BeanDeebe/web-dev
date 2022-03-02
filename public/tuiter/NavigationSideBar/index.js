bf77af358746124e219df80357ab18c740cbecb1
Dean Beebe <beebe.d@northeastern.edu>
2022-03-02 15:29:23 -0500

    A5 Assignment Completed

--- public/tuiter/NavigationSideBar/index.js
+++ public/tuiter/NavigationSideBar/index.js
@@ -1,33 +1,36 @@
 const NavigationSideBar = (activeElement) => {
-
+    $(document).ready(function () {
+        $('#' + activeElement).addClass('active');
+    });
     return (`
-        <div>
-        <ul id="toggle" class="list-group">
+    <div>
+        
+        <ul class="list-group">
                 <li class="list-group-item wd-navBar-left bg-item"><a href="#"><i
                         class="fa-brands fa-xl fa-twitter me-4" style="color:white"></i></a>
                 </li>
-                <li id='test' class=" list-group-item bg-item wd-navBar-left"><a href="../HomeScreen/home.html"><i
+                <li id="home" class="list-group-item bg-item wd-navBar-left"><a href="../HomeScreen/home.html"><i
                       class="fa-solid fa-xl fa-house me-4" style="color:white"></i> <span
                       class="d-none d-xl-inline-flex">Home</span></a></li>
-                <li id="toggle-2" class="list-group-item bg-item wd-navBar-left"><a href="../ExploreScreen/explore.html"><i
+                <li id="explore" class="list-group-item bg-item wd-navBar-left"><a href="../ExploreScreen/explore.html"><i
                         class="fa-solid fa-xl fa-hashtag me-4" style="color:white"></i><span
                         class="d-none d-xl-inline-flex">Explore</span></a></li>
-                <li class="list-group-item bg-item wd-navBar-left"><a  href="#"><i
+                <li id="notifications" class="list-group-item bg-item wd-navBar-left"><a  href="#"><i
                         class="fa-solid fa-xl fa-bell me-4" style="color:white"></i><span
                         class="d-none d-xl-inline-flex"> Notifications</span></a></li>
-                <li class="list-group-item bg-item wd-navBar-left"><a  href="#"><i
+                <li id="messages" class="list-group-item bg-item wd-navBar-left"><a  href="#"><i
                         class="fa-solid fa-xl fa-envelope me-4" style="color:white"></i>
                     <span class="d-none d-xl-inline-flex">Messages</span></a></li>
-                <li class="list-group-item bg-item wd-navBar-left"><a  href="#"><i
+                <li id="bookmarks" class="list-group-item bg-item wd-navBar-left"><a  href="#"><i
                         class="fa-solid fa-xl fa-bookmark me-4" style="color:white"></i>
                     <span class="d-none d-xl-inline-flex">Bookmarks</span></a></li>
-                <li class="list-group-item bg-item wd-navBar-left"><a  href="#"><i
+                <li id="lists" class="list-group-item bg-item wd-navBar-left"><a  href="#"><i
                         class="fa-solid fa-xl fa-list me-4" style="color:white"></i> <span
                         class="d-none d-xl-inline-flex">Lists</span></a></li>
-                <li class="list-group-item bg-item wd-navBar-left"><a  href="#"><i
+                <li id="profile" class="list-group-item bg-item wd-navBar-left"><a  href="#"><i
                         class="fa-solid fa-xl fa-user me-4" style="color:white"></i> <span
                         class="d-none d-xl-inline-flex">Profile</span></a></li>
-                <li class="list-group-item bg-item wd-navBar-left"><a href="#"><i
+                <li id="more" class="list-group-item bg-item wd-navBar-left"><a href="#"><i
                         class="fa-solid fa-xl fa-ellipsis me-4" style="color:white"></i><span
                         class="d-none d-xl-inline-flex">More</span></a></li>
         </ul>
