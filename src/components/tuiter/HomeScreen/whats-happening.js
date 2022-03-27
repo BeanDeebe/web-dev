99e6793eb81c1812ffc1339f666f538a0f5cc00c
Dean Beebe <beebe.d@northeastern.edu>
2022-03-27 14:32:20 -0400

    A7 working

--- src/components/tuiter/HomeScreen/whats-happening.js
+++ src/components/tuiter/HomeScreen/whats-happening.js
@@ -2,13 +2,19 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import profilePic from "../../../profile-pic.jpg"
 import "../../../css/home.css"
 import React, {useState} from "react";
+import {useDispatch} from "react-redux";
 
 const WhatsHappening = () => {
     let [whatsHappening, setWhatsHappening]
         = useState('');
+    const dispatch = useDispatch();
     const tuitClickHandler = () => {
-        console.log(whatsHappening);
+        dispatch({
+            type: 'create-tuit',
+            tuit: whatsHappening
+        });
     }
+
     return (
         <div className="mb-3">
             <div className="wd-accountInfo">
@@ -18,8 +24,12 @@ const WhatsHappening = () => {
                               onChange={(event) =>
                                   setWhatsHappening(event.target.value)} cols={"74"} className="wd-tuitArea mb-1"
                               placeholder={"What's happening?"}/>
-                    <FontAwesomeIcon icon="fa-solid fa-image" className="wd-icon"/>
-                    <button onClick={tuitClickHandler}>
+                    <FontAwesomeIcon icon="fa-solid fa-image" className="wd-icon p-1"/>
+                    <FontAwesomeIcon icon="fa-solid fa-chart-line" className="wd-icon p-1"/>
+                    <FontAwesomeIcon icon="fa-solid fa-face-smile" className="wd-icon p-1"/>
+                    <FontAwesomeIcon icon="fa-solid fa-calendar" className="wd-icon p-1"/>
+                    <button className="wd-color btn btn-primary btn-sm rounded-pill text-right float-end"
+                            onClick={tuitClickHandler}>
                         Tuit
                     </button>
                 </div>
--- src/components/tuiter/reducers/tuits-reducer.js
+++ src/components/tuiter/reducers/tuits-reducer.js
@@ -1,6 +1,38 @@
 import tuits from "../data/tuit.json"
 
-const tuitsReducer = (state = tuits) => {
-    return state;
-}
+const tuitsReducer =
+    (state = tuits, action) => {
+        switch (action.type) {
+            case 'create-tuit':
+                const newTuit = {
+                    _id: (new Date()).getTime() + '',
+                    postedBy: {
+                        "username": "@beandeebe"
+                    },
+                    stats: {
+                        retuits: 36500,
+                        likes: 200000,
+                        replies: 400000
+                    },
+                    avatarIcon: "../images/profile-pic.jpg",
+                    userName: "Dean Beebe",
+                    userHandle: "@beandeebe",
+                    time: "1s",
+                    body: action.tuit,
+                    image: "",
+                    imageDescriptionTitle: "",
+                    imageDescriptionBody: "",
+                    comments: "400K",
+                    retweets: "36.5K",
+                    likes: "200k"
+                }
+                return [
+                    newTuit,
+                    ...state,
+                ];
+            default:
+                return tuits
+        }
+    }
+
 export default tuitsReducer;
